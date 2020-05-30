720e8b535d8673ee5e33ee81b7fc05658c5f2ba5
Don Richard <ricardojaramillo99@hotmail.com>
2020-05-29 21:03:29 -0500

    cambio 2.0

--- Ricardo_Jaramillo.cpp
+++ Ricardo_Jaramillo.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	
+system("color 0A");
+setlocale(LC_ALL, "");
+cout<<"_______________________________________________________________________________________________________"<<endl;
 
